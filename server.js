23ca58bf37ae0795d53b659377f121b81615180e
Kristea Gamel <krgamel@gmail.com>
2019-05-23 20:21:05 -0500

    get endpoint

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 
-const server = express();
+const db = require('./data/dbConfig.js');
 
+const server = express();
 // your code here
+// const { accounts } = db;
+
+server.use(express.json());
+
+
+
+server.get('/accounts', async (req, res) => {
+    try {
+        (req.query)
+       await db.find(accounts);
+        res.status(200).json(db);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retrieving accounts",
+        });
+    }
+});
+
 
 module.exports = server;
